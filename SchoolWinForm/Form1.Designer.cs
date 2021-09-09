e1d0631bd8b439433de269f9ee9779b6a5e74804
sandro.vecchiarelli <sandro.vecchiarelli@pomiager.com>
2021-09-09 12:25:17 +0200

    Last update

--- SchoolWinForm/Form1.Designer.cs
+++ SchoolWinForm/Form1.Designer.cs
@@ -35,15 +35,17 @@ namespace SchoolWinForm
             this.btn_search = new System.Windows.Forms.Button();
             this.txt_search = new System.Windows.Forms.TextBox();
             this.lbl_resultSearch = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // cmb_grades
             // 
             this.cmb_grades.FormattingEnabled = true;
-            this.cmb_grades.Location = new System.Drawing.Point(94, 53);
+            this.cmb_grades.Location = new System.Drawing.Point(96, 82);
             this.cmb_grades.Name = "cmb_grades";
             this.cmb_grades.Size = new System.Drawing.Size(121, 21);
             this.cmb_grades.TabIndex = 0;
+            this.cmb_grades.SelectedIndexChanged += new System.EventHandler(this.cmb_grades_SelectedIndexChanged);
             // 
             // lst_students
             // 
@@ -61,6 +63,7 @@ namespace SchoolWinForm
             this.btn_order.TabIndex = 2;
             this.btn_order.Text = "Order By Name";
             this.btn_order.UseVisualStyleBackColor = true;
+            this.btn_order.Click += new System.EventHandler(this.btn_order_Click);
             // 
             // btn_search
             // 
@@ -70,6 +73,7 @@ namespace SchoolWinForm
             this.btn_search.TabIndex = 3;
             this.btn_search.Text = "Find";
             this.btn_search.UseVisualStyleBackColor = true;
+            this.btn_search.Click += new System.EventHandler(this.btn_search_Click);
             // 
             // txt_search
             // 
@@ -87,11 +91,22 @@ namespace SchoolWinForm
             this.lbl_resultSearch.TabIndex = 5;
             this.lbl_resultSearch.Text = "label1";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(96, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Classe";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.lbl_resultSearch);
             this.Controls.Add(this.txt_search);
             this.Controls.Add(this.btn_search);
@@ -113,6 +128,7 @@ namespace SchoolWinForm
         private System.Windows.Forms.Button btn_search;
         private System.Windows.Forms.TextBox txt_search;
         private System.Windows.Forms.Label lbl_resultSearch;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- SchoolWinForm/Form1.cs
+++ SchoolWinForm/Form1.cs
@@ -19,8 +19,27 @@ namespace SchoolWinForm
         public Form1()
         {
             InitializeComponent();
-            //Inizializzare lista grades
-            //Inizializzare lista students
+            // 1) Inizializzare lista grades
+            // 2) Inizializzare lista students
+
+            // 3) Popolare la combo cmb_grades
+        }
+
+        private void cmb_grades_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Popolare la Listbox con gli studenti della classe scelta
+        }
+
+        private void btn_order_Click(object sender, EventArgs e)
+        {
+            //Usare linq per ordinare la lista in base al campo StudentName
+        }
+
+        private void btn_search_Click(object sender, EventArgs e)
+        {
+            // Leggere il campo text di txt_search (StudentName)
+            // Usare linq per trovare (oppure no) lo StudentName
+            // Visualizzarlo nella label lbl_resultSearch 
         }
     }
 }
